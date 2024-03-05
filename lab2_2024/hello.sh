190cce879582481d212fd1d267485aff6077051d
Andrey Filkin <liamo19@mail.ru>
2024-03-05 22:38:16 +0300

    change first exercise

--- lab2_2024/hello.sh
+++ lab2_2024/hello.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+export DATE=$(date)
+export HOST=$(hostname)
+
 echo "Добро пожаловать на компьютер: $HOST, $LOGNAME"
 echo "Ваш домашний каталог $HOME"
 echo "Ваш командный интерпретатор $SHELL"
