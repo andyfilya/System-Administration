530971c3fce10b5c6ef503696ba4b8b6ad25aa1c
Andrey Filkin <liamo19@mail.ru>
2024-03-06 17:44:47 +0300

    4'd exercise

---
+++ lab2_2024/find-last.sh
@@ -0,0 +1,29 @@
+#!bin/sh
+
+source_directory=$1
+period=$2 #per seconds
+save_directory="$HOME/save_directory"
+
+# make save directory if doesn't exist
+if [ ! -d $save_directory ]; then
+  echo "creation $save_directory save_directory"
+  mkdir $save_directory
+fi 
+
+# check is source_directory exist
+if [ ! -d $source_directory ]; then
+  echo "$source_directory doesn't exist."
+  exit(1)
+fi 
+
+# check all files in source_directory
+for file in $source_directory/*
+do 
+  lastmodified=$(date -r $file +%s)
+  currtime=$(date +%s)
+  elapsedtime=$(( currtime - lastmodified ))
+  if [[ $elapsedtime -le $period ]]; then
+    cp $file "$save_directory/"
+  fi 
+done
+
